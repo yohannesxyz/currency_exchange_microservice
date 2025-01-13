93fbf8070aa2a2f520a7f7739ebae2fe9b7e32b6
Yohannes Haile <yohannessileshi1@gmail.com>
2025-01-13 10:26:53 -0500

    added the config server

--- limits-service/src/main/java/com/currencyExchange/microservices/limits_service/controller/LimitsController.java
+++ limits-service/src/main/java/com/currencyExchange/microservices/limits_service/controller/LimitsController.java
@@ -1,15 +1,21 @@
 package com.currencyExchange.microservices.limits_service.controller;
 
 import com.currencyExchange.microservices.limits_service.bean.Limits;
+import com.currencyExchange.microservices.limits_service.configuration.Configuration;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LimitsController {
 
+    @Autowired
+    Configuration configuration;
+
     @GetMapping("/limits")
     public Limits retriveLimits(){
-        return new Limits(1,1000);
+        return new Limits(configuration.getMinimum(),
+                configuration.getMaximum());
 
     }
 
